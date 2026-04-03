c7749797bb621005ffb6fd1fba54c74d03a3715d
codn0185 <113341200+codn0185@users.noreply.github.com>
2026-04-04 02:36:39 +0900

    feat: app.py 무한루프 설정
    각 프로그램을 종료 후에도 다른 프로그램을 이어서 실행할 수 있도록 무한루프를 추가함

--- app.py
+++ app.py
@@ -3,28 +3,29 @@ from distortion_correction import DistortionCorrection
 
 
 def main():
-    option = int(
-        input(
-            """
-===== Select Option =====
-1: Camera Calibration
-2: Distortion Correction
-=========================
-Option: """
-        )
-    )
+    while True:
+        print("\n\n")
+        print("===== Select Option =====")
+        print("0: Exit Program")
+        print("1: Camera Calibration")
+        print("2: Distortion Correction")
+        print("=========================")
+        option = int(input("Option:"))
 
-    match (option):
-        case 1:
-            print("Program Running: Camera Calibration")
-            app = CameraCalibration()
-            app.run()
-        case 2:
-            print("Program Running: Distortion Correction")
-            app = DistortionCorrection()
-            app.run()
-        case _:
-            print(f"Invalid Option Selected: {option}")
+        match (option):
+            case 0:
+                print("Program Exited")
+                break
+            case 1:
+                print("Program Running: Camera Calibration")
+                app = CameraCalibration()
+                app.run()
+            case 2:
+                print("Program Running: Distortion Correction")
+                app = DistortionCorrection()
+                app.run()
+            case _:
+                print(f"Invalid Option Selected: {option}")
 
 
 if __name__ == "__main__":
